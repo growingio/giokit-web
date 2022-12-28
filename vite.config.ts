c52f38c6d0c305673c4130d587d94174495ad32b
Shenxiaowei <anyi@startdt.com>
2022-12-28 10:19:16 +0800

    chore: 修改编译逻辑

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import VitePluginStyleInject from 'vite-plugin-style-inject';
 // @ts-ignore
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), VitePluginStyleInject()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
       glodash: path.resolve(__dirname, 'src/utils/glodash.ts')
     }
   },
+  server: {
+    open: true
+  },
   build: {
+    cssTarget: 'chrome61',
     rollupOptions: {
       output: {
         entryFileNames: 'assets/gioKit.min.js',
