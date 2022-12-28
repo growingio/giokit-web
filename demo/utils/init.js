aff345b8148bdc5915f669090b59b6775cdbcce7
Shenxiaowei <anyi@startdt.com>
2022-12-28 17:55:59 +0800

    chore: 改造dev和打包逻辑

--- demo/utils/init.js
+++ demo/utils/init.js
@@ -1,23 +1,23 @@
 window._gr_ignore_local_rule = true;
 
-!(function (e, n, t, i, r) {
-  (e[r] =
-    e[r] ||
-    function () {
-      (e[r].q = e[r].q || []).push(arguments);
-    }),
-    (e[r].ef =
-      e[r].ef ||
-      function () {
-        c = arguments[0];
-        (c.eventTime = +Date.now()), (e[r].e = e[r].e || []).push(c);
-      }),
-    e.addEventListener('error', e[r].ef, !0),
-    e.addEventListener('unhandledrejection', e[r].ef, !0),
-    (t = n.createElement('script'));
-  s = n.getElementsByTagName('script')[0];
-  (t.async = !0), (t.src = i), s.parentNode.insertBefore(t, s);
-})(window, document, 'script', './demo/utils/gioPerformance.js', 'gdp');
+// !(function (e, n, t, i, r) {
+//   (e[r] =
+//     e[r] ||
+//     function () {
+//       (e[r].q = e[r].q || []).push(arguments);
+//     }),
+//     (e[r].ef =
+//       e[r].ef ||
+//       function () {
+//         c = arguments[0];
+//         (c.eventTime = +Date.now()), (e[r].e = e[r].e || []).push(c);
+//       }),
+//     e.addEventListener('error', e[r].ef, !0),
+//     e.addEventListener('unhandledrejection', e[r].ef, !0),
+//     (t = n.createElement('script'));
+//   s = n.getElementsByTagName('script')[0];
+//   (t.async = !0), (t.src = i), s.parentNode.insertBefore(t, s);
+// })(window, document, 'script', './demo/utils/gioPerformance.js', 'gdp');
 
 !(function (e, n, t, c, i) {
   (e[i] =
@@ -28,7 +28,7 @@ window._gr_ignore_local_rule = true;
     (t = n.createElement('script'));
   s = n.getElementsByTagName('script')[0];
   (t.async = 1), (t.src = c), s.parentNode.insertBefore(t, s);
-})(window, document, 'script', './demo/utils/gdp-full.js', 'gdp');
+})(window, document, 'script', './utils/gdp-full.js', 'gdp');
 
 gdp('init', '91eaf9b283361032', '95fcb6d4b44fc4cd', {
   debug: true,
--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,36 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import VitePluginStyleInject from 'vite-plugin-style-inject';
-// @ts-ignore
+import packageJson from './package.json';
 import path from 'path';
+import sveltePreprocess from 'svelte-preprocess';
+import VitePluginStyleInject from 'vite-plugin-style-inject';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), VitePluginStyleInject()],
+  plugins: [
+    svelte({
+      preprocess: sveltePreprocess({})
+    }),
+    VitePluginStyleInject()
+  ],
+  define: {
+    __VERSION__: packageJson.version,
+    'process.env.NODE_ENV': '"production"'
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
       glodash: path.resolve(__dirname, 'src/utils/glodash.ts')
     }
   },
-  server: {
-    open: true
-  },
   build: {
-    cssTarget: 'chrome61',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/gioKit.min.js',
-        assetFileNames: `assets/[name].[ext]`
-      }
-    }
+    lib: {
+      name: 'GioKit',
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      formats: ['umd'],
+      fileName: () => 'gioKit.min.js'
+    },
+    target: 'es2015',
+    cssTarget: 'chrome61'
   }
 });
