7235281bb20b52d53b8066ae5d18b7db6caeee07
Shenxiaowei <anyi@startdt.com>
2023-01-09 16:34:21 +0800

    fix: 修复部分问题

--- src/main/ToolContent/index.svelte
+++ src/main/ToolContent/index.svelte
@@ -3,7 +3,6 @@
   import { _activeTool } from '../store';
   import Debugger from '../../modules/debugger/index.svelte';
   import CustomEvents from '../../modules/customEvents/index.svelte';
-  import InitVerification from '../../modules/initVerification/index.svelte';
   import Log from '../../modules/log/index.svelte';
   import Network from '../../modules/network/index.svelte';
   import QuickInit from '../../modules/quickInit/index.svelte';
@@ -16,7 +15,6 @@
    */
   const TOOL_COMPONENTS: any = {
     'custom-events': CustomEvents,
-    'init-verification': InitVerification,
     'quick-init': QuickInit,
     'running-state': RunningState,
     debugger: Debugger,
