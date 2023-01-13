f0d23493d1b4ce8e9dcf47a7b386377455097c28
Shenxiaowei <anyi@startdt.com>
2023-01-13 10:59:20 +0800

    feat: 添加新增storage项功能

--- src/modules/storage/components/AddTool/index.svelte
+++ src/modules/storage/components/AddTool/index.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
   import { Button, Icon } from '@/components';
+  import { _handleType } from '../../store';
+
+  const handleAdd = () => {
+    _handleType.set('add');
+  };
 </script>
 
 <div class="_gk-module-tool">
-  <Button>
+  <Button on:click={handleAdd}>
     <Icon name="addto" />
   </Button>
 </div>
--- src/modules/storage/components/EditModal/index.svelte
+++ src/modules/storage/components/EditModal/index.svelte
@@ -23,7 +23,6 @@
 
   const onSave = () => {
     storages.setItem(targetItem.key, targetItem.value);
-    // @ts-ignore
     refreshStorage();
     onCancel();
   };
@@ -34,13 +33,20 @@
   };
 </script>
 
-<Modal visible={$_handleType === 'edit'} {onCancel} onOk={onSave}>
+<Modal
+  visible={['edit', 'add'].includes($_handleType)}
+  {onCancel}
+  onOk={onSave}
+>
   <div class="_gk-strg-item-tool-edit">
-    <div class="_gk-modal-title"><span>编辑{$_activeStorage}项</span></div>
+    <div class="_gk-modal-title">
+      <span>{$_handleType === 'add' ? '新增' : '编辑'}{$_activeStorage}项</span>
+    </div>
     <div class="_gk-strg-edit-form">
       <FormItem label="名称">
         <Input
           value={targetItem.key ?? ''}
+          disabled={$_handleType === 'edit'}
           on:change={(e) => onChange(e, 'key')}
         />
       </FormItem>
